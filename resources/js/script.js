78ab916dbfb771b7bf580c59b4ca60f2d097af3d
Marin Pavlinov Marinov <wbbmarin5@gmail.com>
2018-10-29 19:36:24 -0400

    added scrolling

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,19 +1,32 @@
-// this will make the selected element red when clicked 
-//$(document).ready(function() {
-//    $('h1').click(function() {
- //       $(this).css('background-color', '#ff0000');
- //   })
-// }); 
-
 $(document).ready(function() {
-
+    /* For the sticky navigation */
     $('.js--section-features').waypoint(function(direction) {
         if (direction == "down") {
             $('nav').addClass('sticky');
-        }
+        } 
         else {
             $('nav').removeClass('sticky');
         }
-    });
+    }, {
+      offset: '60px;'
+    })
 
+    /* scroll buttons */
+    $('.js--scroll-to-plans').click(function () {
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+    })
+
+    $('.js--scroll-to-start').click(function () {
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+    })
+
+
+
+    /* this will make the selected element red when clicked 
+    $(document).ready(function() {
+        $('h1').click(function() {
+            $(this).css('background-color', '#ff0000');
+       })
+     }); 
+     */
 });
