b0bba4d9a14f210efd6ad13f084e140db6a95bd9
Marin Pavlinov Marinov <wbbmarin5@gmail.com>
2018-10-29 20:14:20 -0400

    scrolling down

--- resources/js/script.js
+++ resources/js/script.js
@@ -24,7 +24,22 @@ $(document).ready(function() {
     })
 
 
-
+    /* navigation scroll */
+    $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+          if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+              $('html,body').animate({
+                scrollTop: target.offset().top
+              }, 1000);
+              return false;
+            }
+          }
+        });
+      });
+      
     /* this will make the selected element red when clicked 
     $(document).ready(function() {
         $('h1').click(function() {
