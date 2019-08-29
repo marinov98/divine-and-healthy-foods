2001c994e203dcba0cef5eeba92c34dcd82da758
marinov98 <Marin.Marinov38@myhunter.cuny.edu>
2019-08-29 16:53:47 -0400

    added ReadMe and server logic

---
+++ README.md
@@ -0,0 +1,6 @@
+# Divine Foods
+Website I made with the help of a html/css tutorial on Udemy
+
+# Getting Started
+- Install dependencies `yarn install`
+- Run the app in development mode: 'yarn run dev` or `npm run dev`
---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const logger = require("morgan");
+const path = require("path");
+
+const app = express();
+
+const port = parseInt(process.env.PORT, 10) || 5000;
+
+// Set port
+app.set("port", port);
+
+app.use(express.static("public"));
+
+// Use logger
+app.use(logger("dev"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../", "public", "index.html"));
+});
+
+// Have app listen to port ####
+app.listen(port, () => console.log(`Server up! Listening on port ${port}`));
